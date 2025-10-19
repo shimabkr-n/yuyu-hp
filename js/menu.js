1395315651095d4d343686e99043a20a58d6943c
shimabkr-n <n.shimabkr@gmail.com>
2025-10-19 17:51:23 +0900

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -155,10 +155,10 @@ const renderMenu = (category = 'all') => {
       const formattedDesc = item.desc.replace(/\n/g, '<br>');
 
       container.innerHTML += `
-        <div class="bg-white shadow rounded-lg overflow-hidden flex flex-col md:flex-row relative w-full md:w-[125%]">
+        <div class="bg-white shadow rounded-lg overflow-hidden flex flex-col md:flex-row relative w-full md:w-[105%]">
           ${item.badge ? `<div class="badge">${item.badge}</div>` : ''}
-          <img src="${item.image}" alt="${item.name}" class="w-full md:w-[55%] h-auto object-cover cursor-pointer" onclick="openModal('${item.image}')">
-          <div class="p-4 flex flex-col justify-between">
+          <img src="${item.image}" alt="${item.name}" class="w-full md:w-[53%] h-auto object-cover cursor-pointer" onclick="openModal('${item.image}')">
+          <div class="p-4 flex flex-col justify-between w-auto md:w-[400px]">
             <div>
               <h3 class="text-xl font-bold text-red-700 mb-2">${item.name}</h3>
               <p class="text-sm text-gray-600 mb-2 text-left">${formattedDesc}</p> </div>
